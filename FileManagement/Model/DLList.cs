9934b43a5a2db6a50db74acb9f783c4f7a385eff
M.Mavromanolakis <mauromanolakism@gmail.com>
2024-08-07 15:29:57 +0300

    C# file management

---
+++ FileManagement/Model/DLList.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace FileManagement.Model
+{
+    internal class DLList<T>
+    {
+        public GenericNode<T>? Head { get; set; } = null;
+        public GenericNode<T>? Tail { get; set; } = null;
+
+        public void InsertFirst(T t)
+        {
+            GenericNode<T> tmp = new GenericNode<T>(t)
+            {
+                Next = Head,
+                Prev = null
+            };
+
+            if (IsEmpty())
+            {
+                Tail = tmp;
+            }
+            else
+            {
+                Head!.Prev = tmp;
+            }
+            Head = tmp;
+        }
+
+        public void InsertLast(T t)
+        {
+            if (IsEmpty())
+            {
+                InsertFirst(t);
+                return;
+            }
+
+            GenericNode<T> tmp = new GenericNode<T>(t)
+            {
+                Next = null,
+                Prev = Tail
+            };
+
+            if (Tail != null)
+            {
+                Tail.Next = tmp;
+            }
+            
+            Tail = tmp;
+        }
+
+        public void Traverse(double totalCount)
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            GenericNode<T>? node = Head;
+
+            while (node is not null)
+            {
+                Console.WriteLine($"Item: {node.Value}, Count: {node.Count}, " +
+                    $"Frequency: {(node.Count / totalCount):P2}");
+                node = node.Next;
+            }
+        }
+
+        public GenericNode<T>? GetPosition(T? t)
+        {
+            if (IsEmpty() || t == null)
+            {
+                return null;
+            }
+
+            GenericNode<T>? node = Head;
+
+            while (node is not null)
+            {
+                if (node.Value!.Equals(t))
+                {
+                    return node;
+                }
+                node = node.Next;
+            }
+
+            return null;
+        }
+
+        public void IncreaseCount(GenericNode<T>? node)
+        {
+            if (node is null) return;
+            node.Count++;
+        }
+
+        public void IncreaseCount(T? t)
+        {
+            GenericNode<T>? tmp = GetPosition(t);
+            if (tmp is null)
+            {
+                return;
+            }
+            tmp.Count++;
+        }
+
+        public void SortByValueAsc()
+        {
+            for (GenericNode<T>? iNode = Head; iNode!.Next is not null; iNode = iNode.Next)
+            {
+                T? minVal = iNode.Value;
+                GenericNode<T>? minPos = iNode;
+
+                for (GenericNode<T>? jNode = iNode.Next; jNode is not null; jNode = jNode.Next)
+                {
+                    if (jNode.Value is char)
+                    {
+                        if (Convert.ToChar(jNode.Value) < Convert.ToChar(minVal))
+                        {
+                            minVal = jNode.Value;
+                            minPos = jNode;
+                        }
+                    }
+                }
+
+                Swap(iNode, minPos);
+            }
+        }
+
+        public void SortByCountDesc()
+        {
+            for (GenericNode<T>? iNode = Head; iNode!.Next is not null; iNode = iNode.Next)
+            {
+                int minVal = iNode.Count;
+                GenericNode<T>? minPos = iNode;
+
+                for (GenericNode<T>? jNode = iNode.Next; jNode is not null; jNode = jNode.Next)
+                {                 
+                        if (jNode.Count > minVal)
+                        {
+                            minVal = jNode.Count;
+                            minPos = jNode;
+                        }   
+                }
+                Swap(iNode, minPos);
+            }
+        }
+
+        public void Swap(GenericNode<T>? iNode, GenericNode<T>? jNode)
+        {
+            T? tmpVal = iNode!.Value;
+            int tmpCount = iNode.Count;
+
+            iNode!.Value = jNode!.Value;
+            iNode.Count = jNode.Count;
+
+            jNode.Value = tmpVal;
+            jNode.Count = tmpCount;
+        }
+
+        public bool IsEmpty()
+        {
+            return Head == null && Tail == null;
+        }
+    }
+}
---
+++ FileManagement/Model/GenericNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagement.Model
+{
+    internal class GenericNode<T>
+    {
+        public T? Value { get; set; }
+        public int Count { get; set; }
+        public GenericNode<T>? Next { get; set; }
+        public GenericNode<T>? Prev { get; set; }
+
+        public GenericNode(T value)
+        {
+            Value = value;
+            Count = 1;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
